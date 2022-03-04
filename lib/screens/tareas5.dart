d7ec822d81f9d23ba96fa1bce0c92f0c016941a5
Angie García <dev05@aiatic.com>
2022-03-04 18:28:06 -0500

    boton add

---
+++ lib/screens/tareas5.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class tareas5Screen extends StatelessWidget {
+   
+  const tareas5Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('tareas5Screen'),
+      ),
+    );
+  }
+}
